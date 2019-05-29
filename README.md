3e959a7a2fa6d266e45699118df8948d9e8ee29f
rtomasi75 <roland.tomasi@commonplacerobotics.de>
2019-05-29 16:57:01 +0200

    clean

--- README.md
+++
@@ -1,1 +0,0 @@
-# cpr_robot
--- include/qt_includes.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <qformlayout.h>
-#include <qlabel.h>
-#include <QHBoxLayout>
-#include <qgroupbox.h>
-#include <qpushbutton.h>
-#include <QSlider>
-#include <QGridLayout>
